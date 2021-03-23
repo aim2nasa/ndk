1a0cccd8d09af38bad307a6de0d88d564bb77296
Rossi Kwak <rossi.kwak@gmail.com>
2021-03-23 16:05:50 +0900

    fix 'ld: error: undefined symbol: cyusb_open()'

--- usbHandle/app/src/main/jni/Android.mk
+++ usbHandle/app/src/main/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := testLib
-LOCAL_SRC_FILES := test.cpp
+LOCAL_SRC_FILES := test.cpp libcyusb.cpp
 LOCAL_C_INCLUDES += /home/rossi/libusb/libusb
 
 include $(BUILD_SHARED_LIBRARY)
