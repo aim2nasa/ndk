4143b989f2ca6467e62bfc04dff45a7ff4aa12cf
Rossi Kwak <rossi.kwak@gmail.com>
2021-03-23 15:26:09 +0900

    first function call attempt to cyusb library. Compilation fails at this moment

--- usbHandle/app/src/main/jni/test.cpp
+++ usbHandle/app/src/main/jni/test.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "com_example_usbhandle_MainActivity.h"
+#include <cyusb.h>
 
 JNIEXPORT jint JNICALL Java_com_example_usbhandle_MainActivity_helloNNDK
   (JNIEnv *, jobject, jint v)
@@ -13,7 +14,7 @@ JNIEXPORT jint JNICALL Java_com_example_usbhandle_MainActivity_helloNNDK
 JNIEXPORT jint JNICALL Java_com_example_usbhandle_MainActivity_open
         (JNIEnv *, jobject)
 {
-    return 0;
+    return cyusb_open();
 }
 
 JNIEXPORT void JNICALL Java_com_example_usbhandle_MainActivity_close
