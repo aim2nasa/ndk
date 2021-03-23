52f10c5e91b91450d7ba5316c1d8630f3d374c14
Rossi Kwak <rossi.kwak@gmail.com>
2021-03-23 16:48:41 +0900

    link with libusb-1.0

--- usbHandle/app/src/main/jni/Android.mk
+++ usbHandle/app/src/main/jni/Android.mk
@@ -1,9 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libusb1.0
+LOCAL_SRC_FILES := /home/rossi/libusb/android/libs/armeabi-v7a/libusb1.0.so
+include $(PREBUILT_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := testLib
 LOCAL_SRC_FILES := test.cpp libcyusb.cpp
 LOCAL_C_INCLUDES += /home/rossi/libusb/libusb
+LOCAL_SHARED_LIBRARIES := libusb1.0
 
 include $(BUILD_SHARED_LIBRARY)
