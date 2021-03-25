d029b6a183708e8d16d1cfaf9a8a6837403b8161
Rossi Kwak <rossi.kwak@gmail.com>
2021-03-25 18:46:51 +0900

    toast message on button click

--- usbHandle/app/src/main/java/com/example/usbhandle/MainActivity.java
+++ usbHandle/app/src/main/java/com/example/usbhandle/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.usbhandle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("testLib");
     }
 
+    private Button btnCon;
     public native int helloNNDK(int v);
     public native int open();
     public native void close();
@@ -20,9 +24,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnCon = (Button)findViewById(R.id.button_Connect);
+        btnCon.setEnabled(true);
+
         int result = helloNNDK(5);
 
         ((TextView)findViewById(R.id.tvHello)).setText("result :"+result);
         ((TextView)findViewById(R.id.tvHello)).setText("open :"+open());
+
+        btnCon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Connect clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
