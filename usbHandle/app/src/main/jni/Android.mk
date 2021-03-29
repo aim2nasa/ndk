5290f7796c8e0406d04bd6d8082d1f1f8426efe2
Rossi Kwak <rossi.kwak@gmail.com>
2021-03-29 11:05:58 +0900

    change supporting architecture to 64bit

--- usbHandle/app/src/main/jni/Android.mk
+++ usbHandle/app/src/main/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libusb1.0
-LOCAL_SRC_FILES := /home/rossi/libusb/android/libs/armeabi-v7a/libusb1.0.so
+LOCAL_SRC_FILES := /home/rossi/libusb/android/libs/arm64-v8a/libusb1.0.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
